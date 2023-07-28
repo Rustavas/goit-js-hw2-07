117258d7b727f40a20ae2fdf673e54c8ccb86b07
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-07-28 12:06:58 +0300

    ex-1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const listItems = galleryItems.map(({preview, original, description}) => { 
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', listItems)
+
+gallery.addEventListener('click', onOpenModal)
+
+function onOpenModal(event) {
+  event.preventDefault();
+  // console.dir(event.target.nodeName)
+  const currentItem = event.target
+  if(currentItem.nodeName !== 'IMG'){
+    return;
+  } else {
+    let instance = basicLightbox.create(`
+       <img width="1400" height="900" src="${currentItem.dataset.source}">
+     `);
+    instance.show()
+  };
+};
+
+
+
+
+
+
